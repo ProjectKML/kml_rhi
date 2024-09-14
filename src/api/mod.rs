3273a01ba402b1936a358dd759fb649b0fb05589
BeastLe9enD <beastle9end@outlook.de>
2024-09-14 19:10:04 +0200

    🚧 Work on error type

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,4 +5,11 @@ pub use instance::*;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error {
+    #[cfg(feature = "metal")]
+    #[error("Metal backend: {0}")]
+    MetalBackend(String),
+    #[cfg(feature = "vulkan")]
+    #[error("Vulkan backend: {0}")]
+    VulkanBackend(#[from] ash::vk::Result),
+}
--- src/vulkan/instance.rs
+++ src/vulkan/instance.rs
@@ -3,17 +3,20 @@ use ash::{vk, Entry};
 use crate::{Error, InstanceDesc};
 
 pub struct VulkanInstance {
-    entry_loader: Entry,
+    entry: Entry,
+    instance: ash::Instance,
 }
 
 impl VulkanInstance {
     pub unsafe fn new(desc: &InstanceDesc) -> Result<Self, Error> {
-        let entry = Entry::load().unwrap();
+        let entry_loader = Entry::load().unwrap();
 
         let application_info = vk::ApplicationInfo::default().api_version(vk::API_VERSION_1_3);
 
         let instance_create_info =
             vk::InstanceCreateInfo::default().application_info(&application_info);
+
+        let instance = entry_loader.create_instance(&instance_create_info, None)?;
 
         todo!()
     }
